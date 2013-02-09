c024414c606676536367cd855b86faa4f65cb528
Steven Blair <stevenmblair@gmail.com>
2013-02-09 15:20:43 +0000

    corrected java API example

--- java_interface/Main.java
+++ java_interface/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		rapid61850.start();
 		
-		System.out.println(rapid61850.gse_send_E1Q1SB1_C1_Performance_buf(1, 512));						// send GOOSE packet
+		System.out.println(rapid61850.interface_gse_send_E1Q1SB1_C1_Performance_buf(1, 512));						// send GOOSE packet
 		
 		rapid61850.getE1Q1SB1().getS1().getC1().getMMXUa_1().getMod().setStVal(Mod.MOD_ON);				// interact with IED data model
 		System.out.println(rapid61850.getE1Q1SB1().getS1().getC1().getMMXUa_1().getMod().getStVal());
