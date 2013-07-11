c6f398eb33719c48173217b98cf1922d517094a1
Steven Blair <stevenmblair@gmail.com>
2013-07-11 02:21:21 +0100

    updated LE testing main file

--- c/src/main_SV_LE.c
+++ c/src/main_SV_LE.c
@@ -38,6 +38,18 @@
 #define TWO_PI_OVER_THREE	2.0943951023931954923084289221863
 
 
+int toI(double I, double value) {
+	return (CTYPE_INT32) (I * value / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_1.Amp.sVC.scaleFactor));
+}
+
+int toV(double V, double value) {
+	return (CTYPE_INT32) (V * value / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.sVC.scaleFactor));
+}
+
+double harmonic(double num, double mag, double theta, double phase) {
+	return mag * sin((theta + phase) * num);
+}
+
 int main() {
 	start();    // start IEC 61850 library
 
@@ -47,31 +59,32 @@ int main() {
 	double w;
 	double Vnom = 11000.0;
 	double V = Vnom * sqrt(2) / sqrt(3);
-	double Zmag = 5.0;
+	double Zmag = 8.0;
+	double I = V / Zmag;
 	double phi = 0.2 * PI;
 	double Ts = 1 / (f_nominal * samplesPerCycle);
 	double theta = 0.0;
 	int t = 0;
-	unsigned char muNumber = '1';
+	unsigned int muNumber = 0;
 
 	LE_IED.S1.MUnn.LN0.MSVCB01.ASDU[LE_IED.S1.MUnn.LN0.MSVCB01.ASDUCount].smpRate = f_nominal * samplesPerCycle;
 
 	while (1) {
-		LE_IED.S1.MUnn.LN0.MSVCB01.ASDU[0].svID[9] = muNumber;
+		LE_IED.S1.MUnn.LN0.MSVCB01.ASDU[0].svID[9] = (char) (((int)'0') + muNumber);
 		//phi = (((double) rand()) / ((double) RAND_MAX)) * PI;
 
 		for (t = 0; t < LE_IED.S1.MUnn.LN0.MSVCB01.ASDU[LE_IED.S1.MUnn.LN0.MSVCB01.ASDUCount].smpRate; t++) {
 			w = 2 * PI * f;
 			theta = w * (((double) t) * Ts);
 
-			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.instMag.i = (CTYPE_INT32) (V * sin(theta) / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.sVC.scaleFactor) + (V * 0.25 * sin(theta * 2) / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.sVC.scaleFactor)));
-			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_2.Vol.instMag.i = (CTYPE_INT32) (V * sin(theta - TWO_PI_OVER_THREE) / LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_2.Vol.sVC.scaleFactor) + (V * 0.20 * sin(theta * 3) / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.sVC.scaleFactor));
-			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_3.Vol.instMag.i = (CTYPE_INT32) (V * sin(theta + TWO_PI_OVER_THREE) / LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_3.Vol.sVC.scaleFactor) + (V * 0.10 * sin(theta * 5) / ((double) LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.sVC.scaleFactor));
+			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_1.Vol.instMag.i = toV(V, harmonic(1, 1.0, theta, 0)                   + harmonic(9, muNumber * 0.04, theta, 0)                   + harmonic(11, muNumber * 0.01, theta, 0));
+			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_2.Vol.instMag.i = toV(V, harmonic(1, 1.0, theta, - TWO_PI_OVER_THREE) + harmonic(9, muNumber * 0.02, theta, - TWO_PI_OVER_THREE) + harmonic(13, muNumber * 0.01, theta, - TWO_PI_OVER_THREE));
+			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_3.Vol.instMag.i = toV(V, harmonic(1, 1.0, theta, + TWO_PI_OVER_THREE) + harmonic(9, muNumber * 0.01, theta, + TWO_PI_OVER_THREE) + harmonic(15, muNumber * 0.01, theta, + TWO_PI_OVER_THREE));
 			LE_IED.S1.MUnn.IEC_61850_9_2LETVTR_4.Vol.instMag.i = 0;
 
-			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_1.Amp.instMag.i = (CTYPE_INT32) ((V / Zmag) * sin(theta - phi) / LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_1.Amp.sVC.scaleFactor);
-			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_2.Amp.instMag.i = (CTYPE_INT32) ((V / Zmag) * sin(theta - phi - TWO_PI_OVER_THREE) / LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_2.Amp.sVC.scaleFactor);
-			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_3.Amp.instMag.i = (CTYPE_INT32) ((V / Zmag) * sin(theta - phi + TWO_PI_OVER_THREE) / LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_3.Amp.sVC.scaleFactor);
+			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_1.Amp.instMag.i = toI(I, harmonic(1, 1.0, theta - phi, 0)                   + harmonic(2, muNumber * 0.03, theta - phi, 0)                   + harmonic(3, 0.020, theta - phi, 0)                   + harmonic(5, 0.08, theta - phi, 0)                   + harmonic(7, 0.04, theta - phi, 0)                   + harmonic(9, 0.02, theta - phi, 0));
+			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_2.Amp.instMag.i = toI(I, harmonic(1, 0.9, theta - phi, - TWO_PI_OVER_THREE) + harmonic(2, muNumber * 0.03, theta - phi, - TWO_PI_OVER_THREE) + harmonic(3, 0.020, theta - phi, - TWO_PI_OVER_THREE) + harmonic(5, 0.08, theta - phi, - TWO_PI_OVER_THREE) + harmonic(7, 0.04, theta - phi, - TWO_PI_OVER_THREE) + harmonic(9, 0.02, theta - phi, - TWO_PI_OVER_THREE));
+			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_3.Amp.instMag.i = toI(I, harmonic(1, 0.8, theta - phi, + TWO_PI_OVER_THREE) + harmonic(2, muNumber * 0.03, theta - phi, + TWO_PI_OVER_THREE) + harmonic(3, 0.020, theta - phi, + TWO_PI_OVER_THREE) + harmonic(5, 0.08, theta - phi, + TWO_PI_OVER_THREE) + harmonic(7, 0.04, theta - phi, + TWO_PI_OVER_THREE) + harmonic(9, 0.02, theta - phi, + TWO_PI_OVER_THREE));
 			LE_IED.S1.MUnn.IEC_61850_9_2LETCTR_4.Amp.instMag.i = 0;
 
 			interface_sv_update_LE_IED_MUnn_MSVCB01();    // send SV packet
@@ -79,13 +92,13 @@ int main() {
 
 		// simple method to vary Merging Unit number, and phase angle of current waveforms
 		muNumber++;
-		phi += 0.2 * PI;
-		if (muNumber == '3') {
-			muNumber = '1';
+		phi += 0.10 * PI;
+		if (muNumber == 4) {
+			muNumber = 0;
 			phi = 0.0;
 		}
 
-		usleep(100000);
+		usleep(200000);
 	}
 
 	return 0;
