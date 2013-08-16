948e8a440c15ac454f6759a5d4e6e2a8dbc764f6
Steven Blair <stevenmblair@gmail.com>
2013-08-16 17:52:36 +0100

    fixed Java interface variable naming

--- java_interface/Main.java
+++ java_interface/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		rapid61850.start();
 		
-		System.out.println(rapid61850.interface_gse_send_E1Q1SB1_C1_Performance(1, 512));						// send GOOSE packet
+		System.out.println(rapid61850.interface_gse_send_E1Q1SB1_C1_Performance(1, 512));					// send GOOSE packet
 		
-		rapid61850.getE1Q1SB1().getS1().getC1().getMMXUa_1().getMod().setStVal(Mod.MOD_ON);				// interact with IED data model
+		rapid61850.getE1Q1SB1().getS1().getC1().getMMXUa_1().getMod().setStVal(Mod.MOD_ON_1);				// interact with IED data model
 		System.out.println(rapid61850.getE1Q1SB1().getS1().getC1().getMMXUa_1().getMod().getStVal());
 	}
 }
